c8b6166fb7d6adf8cc00057b8667b2091a46c350
Alireza Alidoust <a.alidoost@gmail.com>
2018-08-13 11:28:47 +0430

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import MainContent from '../components/MainContent';
+
+
+class App extends Component {
+
+  componentWillMount() {
+    //this.props.getRepositoriesByUsername("Facebook");
+  }
+
+  render() {
+
+    
+    return (
+      <div className="App">
+        <MainContent/>
+        
+      </div>
+    );
+  }
+}
+
+
+
+export default App
---
+++ src/microComponents/Input/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import Sizes from '../../design/sizes'
+const styles = theme => ({
+  root: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    borderRadius: Sizes[2]
+  }
+});
+
+class InputComponent extends Component {
+  
+
+  onChange = callback => event => {
+    if (!callback) return;
+
+    const value = event.target.value;
+    callback(value);
+  };
+
+  onClick = callback => event => {
+    if (!callback) return;
+
+    const value = event.target.value;
+    callback(value);
+  };
+
+  onKeyDown = ({ onEnter }) => event => {
+    if (!onEnter) return;
+    
+    const value = event.target.value;
+    if (event.keyCode === 13) return onEnter(value);
+  };
+
+  render() {
+    const { classes, className } = this.props;
+    const classNames = [classes.root, className]
+      .filter(item => item.length)
+      .join(" ");
+
+    const { value , placeholder = '' } = this.props;
+    const { onChange, onClick, onEnter } = this.props;
+    const { InputProps,autoFocus,multiline } = this.props;
+
+    return (
+      <TextField
+        error={this.props.error}
+        label={this.props.label}
+        type={this.props.type}
+        className={classNames}
+        placeholder={placeholder}
+        onKeyDown={this.onKeyDown({ onEnter })}
+        onChange={this.onChange(onChange)}
+        onClick={this.onClick(onClick)}
+        value={value}
+        InputProps={InputProps}
+        autoFocus={autoFocus}
+        multiline={multiline}
+        fullWidth={true}
+      />
+    );
+  }
+}
+
+export default withStyles(styles)(InputComponent);
